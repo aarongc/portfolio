88be656563539b11bcce1b034db08d4d6b715ea9
aaron.castanos <ron.gcastanos@gmail.com>
2020-10-13 13:56:25 +0800

    added graphql queries, offline support and seo implementations

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,15 +1,37 @@
 import React from "react"
+import { graphql } from "gatsby"
 import Layout from "../components/layout"
+import SEO from "../components/seo"
 
-const About = () => {
+const About = ({ data }) => {
+    const companies = data.site.siteMetadata.companies.map(({ company }) => {
+            if (company !== 'inspire team') 
+                return <p key={company}>{company}</p>
+        }       
+    )
+
     return (
         <Layout>
-            <h1>About me</h1>
-            <p>
-                I'm good enough, I'm smart enough, and gosh darn it, people like me!
-            </p>
+            <SEO title={data.site.siteMetadata.title} description={data.site.siteMetadata.description} />
+            <h1>{data.site.siteMetadata.title} </h1>
+            <p>{data.site.siteMetadata.description}</p>
+            {companies}
         </Layout>
     )
 }
+
+export const query = graphql `
+    query {
+        site {
+            siteMetadata {
+                title
+                description
+                companies { 
+                    company
+                }
+            }
+        }
+    }
+`
 
 export default About
