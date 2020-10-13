7e9232601c686ccbf95fc33bc9032a27417bbb1d
aaron.castanos <ron.gcastanos@gmail.com>
2020-10-13 14:27:23 +0800

    removed temp the icon option config

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,6 +24,13 @@ module.exports = {
   },
   plugins: [
     {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`,
+      },
+    },
+    {
       resolve: `gatsby-plugin-typography`,
       options: {
         pathToConfigModule: `src/utils/typography`
@@ -37,10 +44,7 @@ module.exports = {
         start_url: `/`,
         background_color: `#6b37bf`,
         theme_color: `#6b37bf`,
-        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
-        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
-        display: `standalone`,
-        // icon: `src/images/icon.png`, // This path is relative to the root of the site.
+        display: `standalone`
       },
     },
     `gatsby-plugin-offline`,
